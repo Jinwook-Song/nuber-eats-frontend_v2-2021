68ddee2c3ba92b275dddaa22ec095ae6a0aa0b33
jinwook-song <wlsdnr129@naver.com>
2021-12-01 02:30:15 +0900

    Form Design

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,24 @@
 function Login() {
-  return <div>Login Page</div>;
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-md py-8 rounded-lg text-center">
+        <h3 className=" text-3xl text-gray-800">Log In</h3>
+        <form className="flex flex-col mt-5 px-5">
+          <input
+            placeholder="Email"
+            className="mb-3 bg-gray-100 shadow-inner   border-2 focus:border-opacity-60 focus:border-green-600 focus:outline-none  py-3 px-5 rounded-lg"
+          />
+          <input
+            placeholder="Password"
+            className=" bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600  py-3 px-5 rounded-lg"
+          />
+          <button className="mt-3 py-3 px-5 bg-gray-800 text-white text-lg rounded-lg focus:outline-none hover:opacity-90">
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
