a6e6b1169b44f3fb6a23d35ed4c96b1d9d6a4f08
jinwook-song <wlsdnr129@naver.com>
2021-12-01 01:51:49 +0900

    React Router Dom

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,69 +1,15 @@
-import { useForm } from "react-hook-form";
-
-interface IForm {
-  email: string;
-  password: string;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateAccount from "../pages/create-account";
+import Login from "../pages/login";
 
 function LoggedOutRouter() {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>();
-
-  const onSubmit = () => {
-    console.log(watch());
-    alert("Create account 🎉");
-  };
-  const onInvalid = () => {
-    console.log("Can't create account.");
-  };
-
   return (
-    <div>
-      <h1>LogIn First</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register("email", {
-              required: "Email is required.",
-              pattern: {
-                value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-                message: "Only gmail allowed.",
-              },
-            })}
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="font-bold text-red-600">
-              {errors.email?.message}
-            </span>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("password", {
-              required: "Password is required.",
-              minLength: {
-                value: 4,
-                message: "Password should contain 4 characters.",
-              },
-            })}
-            type="password"
-            placeholder="password"
-          />
-          {errors.password?.message && (
-            <span className="font-bold text-red-600">
-              {errors.password?.message}
-            </span>
-          )}
-        </div>
-        <button className="bg-yellow-300 text-pink-900">Submit</button>
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
