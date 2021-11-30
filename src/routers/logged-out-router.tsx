ae28c4795e501ac808be1c254ef7d541ac4083bf
jinwook-song <wlsdnr129@naver.com>
2021-12-01 01:26:56 +0900

    React Hook Form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,13 +1,68 @@
-import { isLoggedInVar } from "../apollo";
+import { useForm } from "react-hook-form";
+
+interface IForm {
+  email: string;
+  password: string;
+}
 
 function LoggedOutRouter() {
-  const onClick = () => {
-    isLoggedInVar(true);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit = () => {
+    console.log(watch());
+    alert("Create account 🎉");
   };
+  const onInvalid = () => {
+    console.log("Can't create account.");
+  };
+
   return (
     <div>
-      <h1>Logged Out</h1>
-      <button onClick={onClick}>Login &rarr;</button>
+      <h1>LogIn First</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register("email", {
+              required: "Email is required.",
+              pattern: {
+                value: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+                message: "Only gmail allowed.",
+              },
+            })}
+            type="email"
+            placeholder="email"
+          />
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+        </div>
+        <div>
+          <input
+            {...register("password", {
+              required: "Password is required.",
+              minLength: {
+                value: 4,
+                message: "Password should contain 4 characters.",
+              },
+            })}
+            type="password"
+            placeholder="password"
+          />
+          {errors.password?.message && (
+            <span className="font-bold text-red-600">
+              {errors.password?.message}
+            </span>
+          )}
+        </div>
+        <button className="bg-yellow-300 text-pink-900">Submit</button>
+      </form>
     </div>
   );
 }
