6cb88515d9df90d226cf6e34dd27aaba1b693380
jinwook-song <wlsdnr129@naver.com>
2021-12-03 00:32:37 +0900

    Get All Restaurants and Categories

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,6 +1,55 @@
+import { gql, useQuery } from "@apollo/client";
 import Banner from "../../components/banner";
+import {
+  RestaurantsPageQuery,
+  RestaurantsPageQueryVariables,
+} from "../../__generated__/RestaurantsPageQuery";
+
+const RESTAURANTS_QUERY = gql`
+  query RestaurantsPageQuery($input: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImg
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $input) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImg
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
 
 function Restaurants() {
+  const { data, loading } = useQuery<
+    RestaurantsPageQuery,
+    RestaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, {
+    variables: {
+      input: {
+        page: 1,
+      },
+    },
+  });
+
+  console.log(data, loading);
   return (
     <>
       <Banner />
