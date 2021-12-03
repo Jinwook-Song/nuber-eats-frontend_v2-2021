b8074c920d8a3765845e4b5567f04005ede49dad
jinwook-song <wlsdnr129@naver.com>
2021-12-04 00:49:09 +0900

    Responsive Header with Scroll

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,11 @@ import { Link, useNavigate } from "react-router-dom";
 import useMyProfile from "../hooks/useMyProfile";
 import { authTokenVar, isLoggedInVar } from "../apollo";
 import { UBER_AUTH_TOKEN } from "../types";
+import { useEffect, useState } from "react";
 
 function Header() {
   const { data, refetch } = useMyProfile();
+  const [scrollY, setScrollY] = useState(0);
   const navigate = useNavigate();
 
   refetch();
@@ -21,14 +23,30 @@ function Header() {
     navigate("/");
   };
 
+  const onScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
     <>
       {!data?.myProfile.verified && (
-        <div className="bg-red-500 p-8 text-center text-lg text-white opacity-95 fixed top-0 w-full">
+        <div className="bg-red-500 p-8 text-center text-lg text-white opacity-95 fixed top-0 w-full z-20">
           <span>Please verify your email.</span>
         </div>
       )}
-      <header className="py-8 fixed top-0 w-full">
+      <header
+        className="py-8 fixed top-0 w-full z-10 transition-colors"
+        style={{
+          backgroundColor: scrollY >= 100 ? "white" : "transparent",
+        }}
+      >
         <div className="w-full px-4 xl:px-2 max-w-screen-xl mx-auto flex justify-between items-center">
           <Link to="/">
             <img src={Logo} className="w-40" alt="uber eats" />
